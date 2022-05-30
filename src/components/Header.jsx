055722de2b783020b7932df8ef5a8a248bcbef63
Vitor <tutornews2@gmail.com>
2022-05-30 15:30:55 -0300

    :lipstick: style: Adiciona css do componente header e alterações no mesmo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,38 @@ const Header = () => {
         <header className="header">
             <nav className="nav container">
                 <a href="#" className="nav_logo">
-                    Viagem <i className='bx bxs-plane-alt'></i>
+                    Viagem
+                    <i className='bx bxs-plane-alt'></i>
                 </a>
                 <div className="nav_menu">
                     <ul className="nav_list">
-                        <li className="nav_item"><a href="#" className="nav_link"><span></span></a></li>
-                        <li className="nav_item"><a href="#" className="nav_link"><span></span></a></li>
-                        <li className="nav_item"><a href="#" className="nav_link"><span></span></a></li>
-                        <li className="nav_item"><a href="#" className="nav_link"><span></span></a></li>
+                        <li className="nav_item">
+                            <a href="#home" className="nav_link">
+                                <i className='bx bx-home-alt-2'></i>
+                                <span>Início</span>
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#popular" className="nav_link">
+                                <i className='bx bx-map-alt'></i>
+                                <span>Lugares</span>
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#value" className="nav_link">
+                                <i className='bx bx-trip'></i>
+                                <span>Passagens</span>
+                            </a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#contact" className="nav_link">
+                                <i className='bx bx-phone'></i>
+                                <span>Contato</span>
+                            </a>
+                        </li>
                     </ul>
                 </div>
+                <a href="#" className="button nav_button">Cadastrar</a>
             </nav>
         </header>
     );
