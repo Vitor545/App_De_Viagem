61e65afbbf41360317a48c5de29ff7e4b15d2b4a
Vitor <tutornews2@gmail.com>
2022-06-01 15:10:31 -0300

    :sparkles: feat: Adiciona accordion.

---
+++ src/components/Accordion3.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, useState }  from "react";
+
+const Accordion3 = () => {
+    const value = useRef(null);
+    const valueItem = useRef(null);
+    const [accordionOpen, serAccconrdionOpen] = useState(false);
+
+    const onClickValue = () => {
+        const accordionContent = value.current;
+        const accordion = valueItem.current;
+        if (accordion.classList.contains('accordion_open')) {
+            accordionContent.removeAttribute('style');
+            serAccconrdionOpen(false)
+        } else {
+            accordionContent.style.height = accordionContent.scrollHeight + 'px'
+            serAccconrdionOpen(true)
+        }
+    }
+    return(
+        <div
+            className={accordionOpen ? "value_accordion_item accordion_open" :"value_accordion_item"}
+            ref={valueItem}
+        >
+                            <header
+                                className="value_accordion_header"
+                                onClick={onClickValue}
+                            >
+                                <i className='bx bxs-bar-chart-square value_accordion_icon'></i>
+                                <h3 className="value_accordion_title">
+                                    Segurança dos seus dados
+                                </h3>
+                                <div className="value_accordion_arrow">
+                                    <i className='bx bxs-down-arrow' ></i>
+                                </div>
+                            </header>
+                            <div
+                                className="value_accordion_content"
+                                ref={value}
+                            >
+                                <p className="value_accordion_description">
+                                O preço que fornecemos é o melhor para você,
+                                garantimos nenhuma alteração de preço em sua viajem devido
+                                a vários custos inesperados que podem surgir.
+                                </p>
+                            </div>
+                        </div>
+    );
+}
+
+export default Accordion3;
--- src/page/Value.jsx
+++ src/page/Value.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Accordion from "../components/Accordion";
+import Accordion2 from "../components/Accordion2";
+import Accordion3 from "../components/Accordion3";
+import Accordion4 from "../components/Accordion4";
 import Values from '../img/value.jpg';
 
 const Value = () => {
+    
     return(
         <section className="value section" id="value">
             <div className="value_container container grid">
@@ -23,78 +28,10 @@ const Value = () => {
                         </p>
                     </div>
                     <div className="value_accordion">
-                        <div className="value_accordion_item">
-                            <header className="value_accordion_header">
-                                <i className='bx bxs-shield-x value_accordion_icon'></i>
-                                <h3 className="value_accordion_title">
-                                    As melhores taxas de juros do mercado
-                                </h3>
-                                <div className="value_accordion_arrow">
-                                    <i className='bx bxs-down-arrow' ></i>
-                                </div>
-                            </header>
-                            <div className="value_accordion_content">
-                                <p className="value_accordion_description">
-                                O preço que fornecemos é o melhor para você,
-                                garantimos nenhuma alteração de preço em sua viajem devido
-                                a vários custos inesperados que podem surgir.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value_accordion_item">
-                            <header className="value_accordion_header">
-                                <i className='bx bxs-x-square value_accordion_icon'></i>
-                                <h3 className="value_accordion_title">
-                                    Prevenir preços instáveis
-                                </h3>
-                                <div className="value_accordion_arrow">
-                                    <i className='bx bxs-down-arrow' ></i>
-                                </div>
-                            </header>
-                            <div className="value_accordion_content">
-                                <p className="value_accordion_description">
-                                O preço que fornecemos é o melhor para você,
-                                garantimos nenhuma alteração de preço em sua viajem devido
-                                a vários custos inesperados que podem surgir.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value_accordion_item">
-                            <header className="value_accordion_header">
-                                <i className='bx bxs-bar-chart-square value_accordion_icon'></i>
-                                <h3 className="value_accordion_title">
-                                    Segurança dos seus dados
-                                </h3>
-                                <div className="value_accordion_arrow">
-                                    <i className='bx bxs-down-arrow' ></i>
-                                </div>
-                            </header>
-                            <div className="value_accordion_content">
-                                <p className="value_accordion_description">
-                                O preço que fornecemos é o melhor para você,
-                                garantimos nenhuma alteração de preço em sua viajem devido
-                                a vários custos inesperados que podem surgir.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value_accordion_item">
-                            <header className="value_accordion_header">
-                                <i className='bx bxs-check-square value_accordion_icon' ></i>
-                                <h3 className="value_accordion_title">
-                                    Segurança dos seus dados
-                                </h3>
-                                <div className="value_accordion_arrow">
-                                    <i className='bx bxs-down-arrow' ></i>
-                                </div>
-                            </header>
-                            <div className="value_accordion_content">
-                                <p className="value_accordion_description">
-                                O preço que fornecemos é o melhor para você,
-                                garantimos nenhuma alteração de preço em sua viajem devido
-                                a vários custos inesperados que podem surgir.
-                                </p>
-                            </div>
-                        </div>
+                        <Accordion/>
+                        <Accordion2/>
+                        <Accordion3/>
+                        <Accordion4/>
                     </div>
                 </div>
             </div>
