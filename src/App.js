6fe7abc7676572a66aeda1063576ae41651823ef
Vitor <tutornews2@gmail.com>
2022-06-02 14:58:36 -0300

    :sparkles: feat: Adiciona botão para voltar ao inicio..

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header"
 import Main from "./components/Main"
+import ScrollUp from "./components/ScrollUp";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Main />
       <Footer />
+      <ScrollUp />
     </React.Fragment>
   );
 }
---
+++ src/components/ScrollUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ScrollUp = () => {
+    return(
+        <a href="#" className="scrollup" id="scroll-up">
+            <i className='bx bx-chevrons-up'></i>
+        </a>
+    );
+}
+
+export default ScrollUp;
